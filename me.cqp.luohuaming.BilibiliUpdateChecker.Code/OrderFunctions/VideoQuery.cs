f535c63d698723cb2b42084fa109d22022dfbbfd
Mr.喜 <50934714+Hellobaka@users.noreply.github.com>
2022-06-21 21:58:24 +0800

    视频解析功能

---
+++ me.cqp.luohuaming.BilibiliUpdateChecker.Code/OrderFunctions/VideoQuery.cs
@@ -0,0 +1,80 @@
+﻿using me.cqp.luohuaming.BilibiliUpdateChecker.Sdk.Cqp.EventArgs;
+using me.cqp.luohuaming.BilibiliUpdateChecker.PublicInfos;
+using BilibiliMonitor.BilibiliAPI;
+using me.cqp.luohuaming.BilibiliUpdateChecker.Sdk.Cqp;
+using System.Text.RegularExpressions;
+
+namespace me.cqp.luohuaming.BilibiliUpdateChecker.Code.OrderFunctions
+{
+    public class VideoQuery : IOrderModel
+    {
+        public bool ImplementFlag { get; set; } = true;
+
+        public string GetOrderStr() => "#解析";
+
+        public bool Judge(string destStr)
+        {
+            return true;
+        }
+
+        public FunctionResult Progress(CQGroupMessageEventArgs e)//群聊处理
+        {
+            FunctionResult result = new FunctionResult
+            {
+                Result = true,
+                SendFlag = true,
+            };
+            SendText sendText = new SendText
+            {
+                SendID = e.FromGroup,
+            };
+            result.SendObject.Add(sendText);
+            if (e.FromGroup != 891787846) { result.SendFlag = false; return result; }
+
+            string vid;
+            if (e.Message.Text.Contains("[CQ:rich,") && e.Message.Text.Contains("b23.tv"))
+            {
+                vid = Videos.ParseURLFromXML(e.Message.Text);
+            }
+            else
+            {
+                string t = Regex.Replace(e.Message.Text, "\\[CQ:.*\\]", "");
+                vid = Videos.ParseURL(t); 
+            }
+            //e.CQLog.Debug("视频解析", vid);
+            if (string.IsNullOrEmpty(vid))
+            {
+                result.SendFlag = false;
+                result.Result = false;
+                return result;
+            }
+            try
+            {
+                sendText.MsgToSend.Add(CQApi.CQCode_Image(Videos.DrawVideoPic(vid)).ToString());
+            }
+            catch
+            {
+                result.SendFlag = false;
+                result.Result = false;
+            }
+            return result;
+        }
+
+        public FunctionResult Progress(CQPrivateMessageEventArgs e)//私聊处理
+        {
+            FunctionResult result = new FunctionResult
+            {
+                Result = false,
+                SendFlag = false,
+            };
+            SendText sendText = new SendText
+            {
+                SendID = e.FromQQ,
+            };
+
+            sendText.MsgToSend.Add("这里输入需要发送的文本");
+            result.SendObject.Add(sendText);
+            return result;
+        }
+    }
+}
